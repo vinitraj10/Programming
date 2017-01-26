c99583b97b457bfea1ab519b620bc1272d53df6f
VINIT RAJ <ent.vinit01@gmail.com>
2017-01-26 13:33:56 +0530

    Factotrial Program in java
    this program can calculate factorial upto 33 because the fact is integer variable so after 33 the output will go beyond the limit of  integral variable

--- Fact.java
+++ Fact.java
@@ -3,7 +3,7 @@ public class Fact{
   public static void main(String args[]){
     Scanner input= new Scanner(System.in);
     int i,num,fact=1;
-    System.out.println("Enter the Number to caluculate its Factorial");
+    System.out.println("Enter the No. to caluculate its Factorial");
     num=input.nextInt();
     for(i=1;i<=num;i++){
         fact=fact*i;
