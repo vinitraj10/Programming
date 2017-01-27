bceb1657ad640f1702168d76fe997b15609bcba9
VINIT RAJ <ent.vinit01@gmail.com>
2017-01-27 18:12:32 +0530

    Used multiple Classes

--- Multiple-classes/multi.java
+++ Multiple-classes/multi.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+class Multi{
+  public static void main(String args[]){
+      Scanner input= new Scanner(System.in);
+      MultiClass object = new MultiClass();
+      System.out.println("Enter the name here :");
+      String name = input.nextLine();
+      object.simpleMessege(name);
+	  object.saying();
+  }
+}
