864ac3c609dec9c3394cb6917201ad03d2275139
VINIT RAJ <ent.vinit01@gmail.com>
2017-01-28 17:21:37 +0530

    Array Initialization and Declaration in java

---
+++ Day-3/Array1.java
@@ -0,0 +1,11 @@
+public class Array1{
+	public static void main(String args[]){
+		int arr[]= new int [10]; //1st way to declare an Array.
+		arr[0]=1;
+		arr[1]=2;
+		arr[9]=3;
+		System.out.println(arr[1] + " is 2nd Element of Array-1 By 1st way Of Declaration");
+		int arr2[]={1,2,3,4,5,6,7,8,9};
+		System.out.println(arr2[7] + " is 8 Element Of Array-2 By 2nd way Declaration");
+	}
+}
