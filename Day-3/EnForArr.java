33091828473de408de6f9a40cdccb41320546b2f
VINIT RAJ <ent.vinit01@gmail.com>
2017-01-28 20:05:30 +0530

    Enhanced For loop in array

---
+++ Day-3/EnForArr.java
@@ -0,0 +1,10 @@
+public class EnForArr{
+	public static void main(String args[]){
+		int arr[]={5,3,4,6,7,9};
+		int total=0;
+		for(int x:arr){
+			total+=x;
+		}
+		System.out.println("Using Enhanced Loop We found Sum: " + total );
+	}
+}
