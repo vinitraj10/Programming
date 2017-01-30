60bf1e743b401ff1f0d032eb481f13c7655fe90a
vinitraj10 <ent.vinit01@gmail.com>
2017-01-30 14:07:10 +0530

    Array-Concepts In Day5

--- Day-4/Fibonacci.java
+++ Day-4/Fibonacci.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
 public class Fibonacci{
+	private static long fib(long n){
+		if(n < 2)
+        	return n;
+    	long  a = 0,b = 1;
+    	long ans=0;
+    	int i = 1;
+   		 while(i < n) {
+        	ans = (a+b) % 1000000007;
+        	a = b;
+        	b = ans;
+        	i++;
+    	}
+    	return ans;
+    }
 	public static void main(String args[]){
 		Scanner input = new Scanner(System.in);
-		int n,fib;
-		System.out.println("Which Term No of Fibonacci do you want To print");
-		n=input.nextInt();
-		fib=fib(n);
-		System.out.println("That term is " + fib);
+		long n,a;
+			System.out.println("Which Term of Fibonacci Series You want to Print:");
+			n=input.nextLong();
+			a=fib(n);
+			System.out.println(a);
+		}
 	}
-	private static int fib(int n){
-		if(n==0)
-			return 0;
-		else if(n==1)
-			return 1;
-		else
-			return fib(n-1)+fib(n-2);
-	}
-}
+
