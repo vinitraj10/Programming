0e289479d39b1e1b2fdb98b8d5ded893f28a78de
vinitraj10 <ent.vinit01@gmail.com>
2017-02-02 18:30:25 +0530

    Min-Max Problem

---
+++ HackerEarth-Problems/MinMax.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class MinMax{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        int n,max=0,min=999,flag=0;
+        n=input.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = input.nextInt();
+            if(a[i]>max){
+                max=a[i];
+            }
+            if(a[i]<min){
+                min=a[i];
+            }
+        }
+        for(int j=min;j<=max;j++){
+            flag=0;
+            for(int i = 0;i<n;i++){
+                if(a[i]==j){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)
+                break;
+        }
+        if(flag==0)
+        System.out.printf("NO\n");
+        else
+        System.out.printf("YES\n");
+
+    }
+}
